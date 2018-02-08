3297e83444bf122edab9a77edc78a176c68b101b
Anh <anh.truong.07@cnu.edu>
2018-02-08 05:04:04 -0500

    seperate toggles for latest and all

--- assets/javascript/gallery.js
+++ assets/javascript/gallery.js
@@ -1,12 +1,16 @@
 $( document ).ready(function() {
 
   $("#show-all").click(function() {
-    $(".container #gallery").removeClass("hidden")
+    $(".container #gallery-all").removeClass("hidden")
+    $(".container #gallery-latest").removeClass("hidden")
     $("#show-all").addClass("hidden")
     // alert("butts");
   });
 
   $("#show-latest").click(function() {
-    $(".container #gallery").removeClass("hidden")
+    $(".container #gallery-latest").removeClass("hidden")
     })
+
+  $('.')   
+
   });
